633e34c9673df866027d0568c94ea25a6c14487d
AliZ <aliz.az2000@gmail.com>
2022-10-06 08:09:09 +0330

    starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-between bg-green-600 p-7 mt-2 m-4">
+        <h1 className="text-3xl font-bold underline ">
+    hi
+        </h1>
+        <h1 className="text-3xl font-bold underline ">
+    hi3333
+        </h1>
     </div>
   );
 }

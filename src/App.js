640e5ea245ddc3ece88d9f627d663338f6b8c982
AliZ <aliz.az2000@gmail.com>
2022-10-07 07:45:53 +0330

    header and navbar components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import Navbar from "./components/Header/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App flex justify-between bg-green-600 p-7 mt-2 m-4">
-        <h1 className="text-3xl font-bold underline ">
-    hi
-        </h1>
-        <h1 className="text-3xl font-bold underline ">
-    hi3333
-        </h1>
+    <div className="bg-black text-white">
+    <Navbar/>
+      <div></div> <p className="bg-slate-800">ali ali ali</p>
+      <div className="h-screen"></div>
     </div>
   );
 }

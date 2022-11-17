3b5b2f1fbe9de5d44410f031da6cc91dc15534e3
AliZ <aliz.az2000@gmail.com>
2022-11-17 15:57:13 +0330

    axios services created

---
+++ src/services/admin/blog.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:9000";
+
+//* =======Blog=======
+//  @desc   Admin Create Post
+//  @route  POST /admin/add-post
+export const createPost = (post) => {
+  const url = `${SERVER_URL}/admin/add-post`;
+  return axios.post(url, post);
+};
+
+//  @desc   Admin Edit Post
+//  @route  PUT /admin/edit-post/:id
+export const editPost = (postId, post) => {
+  const url = `${SERVER_URL}/admin/edit-post/${postId}`;
+  return axios.put(url, post);
+};
+
+//  @desc   Admin Delete Post
+//  @route  DELETE /admin/delete-post/:id
+export const deletePost = (postId) => {
+  const url = `${SERVER_URL}/admin/delete-post/${postId}`;
+  return axios.delete(url);
+};
+
+//* =======Movies=======
+//  @desc   Admin Create Movie
+//  @route  POST /admin/add-movie
+export const createMovie = (movie) => {
+  const url = `${SERVER_URL}/admin/add-movie`;
+  return axios.post(url, movie);
+};
+
+//  @desc   Admin Edit Movie
+//  @route  PUT /admin/edit-movie/:id
+export const editMovie = (movieId, movie) => {
+  const url = `${SERVER_URL}/admin/edit-movie/${movieId}`;
+  return axios.put(url, movie);
+};
+
+//  @desc   Admin Delete Movie
+//  @route  DELETE /admin/delete-movie/:id
+export const deleteMovie = (movieId) => {
+  const url = `${SERVER_URL}/admin/delete-movie/${movieId}`;
+  return axios.delete(url);
+};
+
+//* =======Books=======
+//  @desc   Admin Create Book
+//  @route  POST /admin/add-book
+export const createBook = (book) => {
+  const url = `${SERVER_URL}/admin/add-book`;
+  return axios.post(url, book);
+};
+
+//  @desc   Admin Edit Book
+//  @route  PUT /admin/edit-book/:id
+export const editBook = (bookId, book) => {
+  const url = `${SERVER_URL}/admin/edit-book/${bookId}`;
+  return axios.put(url, book);
+};
+
+//  @desc   Admin Delete Book
+//  @route  DELETE /admin/delete-book/:id
+export const deleteBook = (bookId) => {
+  const url = `${SERVER_URL}/admin/delete-book/${bookId}`;
+  return axios.delete(url);
+};
+
+//* =======Gallery=======
+//  @desc   Admin Create Photo
+//  @route  POST /admin/add-photo
+export const createPhoto = (photo) => {
+  const url = `${SERVER_URL}/admin/add-photo`;
+  return axios.post(url, photo);
+};
+
+//  @desc   Admin Edit Photo
+//  @route  PUT /admin/edit-photo/:id
+export const editPhoto = (photoId, photo) => {
+  const url = `${SERVER_URL}/admin/edit-photo/${photoId}`;
+  return axios.put(url, photo);
+};
+
+//  @desc   Admin Delete Photo
+//  @route  DELETE /admin/delete-photo/:id
+export const deletePhoto = (photoId) => {
+  const url = `${SERVER_URL}/admin/delete-photo/${photoId}`;
+  return axios.delete(url);
+};
---
+++ src/services/web/blog.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:9000";
+
+//  @desc   Weblog Home Page
+//  @route  GET /
+//! Home Page
+
+//  @desc   Get All Posts
+//  @route  GET /blog
+export const getAllPosts = () => {
+  const url = `${SERVER_URL}/blog`;
+  return axios.get(url);
+};
+
+//  @desc   Get All Movies
+//  @route  GET /movies
+export const getMovies = () => {
+  const url = `${SERVER_URL}/movies`;
+  return axios.get(url);
+};
+
+//  @desc   Get All Books
+//  @route  GET /books
+export const getBooks = () => {
+  const url = `${SERVER_URL}/books`;
+  return axios.get(url);
+};
+
+//  @desc   Get All Photoes
+//  @route  GET /gallery
+export const getGallery = () => {
+  const url = `${SERVER_URL}/gallery`;
+  return axios.get(url);
+};
+
+//  @desc   Get Single post With postId
+//  @route  GET /post/:id
+export const getPost = (postId) => {
+  const url = `${SERVER_URL}/post/${postId}`;
+  return axios.get(url);
+};
+
+//  @desc   Add Post Comment
+//  @route  POST /post/add-comment
+export const createComment = (comment) => {
+  const url = `${SERVER_URL}/post/add-comment`;
+  return axios.post(url, comment);
+};
+
+//  @desc   Edit Post Comment
+//  @route  PUT /post/edit-comment/:commentId
+export const editComment = (commentId, comment) => {
+  const url = `${SERVER_URL}/post/edit-comment/${commentId}`;
+  return axios.put(url, comment);
+};
+//  @desc   Delete Post Comment
+//  @route  DELETE /post/delete-comment/:commentId
+export const deleteComment = (commentId) => {
+  const url = `${SERVER_URL}/post/delete-post/${commentId}`;
+  return axios.delete(url);
+};
+
+//  @desc   get post comments
+//  @route  POST /post/comments/:postid
+export const getComments = (postId) => {
+  const url = `${SERVER_URL}/post/comments/${postId}`;
+  return axios.get(url);
+};
